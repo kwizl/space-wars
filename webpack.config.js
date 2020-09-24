a7c998efece89b8165508dbcc846c39042862584
kwizl <karis.njoroge@gmail.com>
2020-09-24 23:51:14 +0300

    Test webpack functionality

--- webpack.config.js
+++ webpack.config.js
@@ -10,25 +10,16 @@ module.exports = {
     publicPath: '/build/',
     filename: 'js/main.js'
   },
+
   module: {
     rules: [
       {
         test: [ /\.vert$/, /\.frag$/ ],
         use: 'raw-loader'
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread']
-          }
-        }
       }
     ]
   },
+
   plugins: [
     new webpack.DefinePlugin({
       'CANVAS_RENDERER': JSON.stringify(true),
